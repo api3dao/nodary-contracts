7cbd6126e4d5e418918b65180d7978d20655ae40
bbenligiray <bbenligiray@gmail.com>
2023-09-29 18:14:56 +0300

    Bump @api3/chains

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,37 +1,12 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-etherscan');
 require('hardhat-deploy');
-const api3Chains = require('@api3/chains');
+const { hardhatConfig } = require('@api3/chains');
 require('dotenv').config();
 
-const { apiKey: etherscanApiKey, customChains: etherscanCustomChains } = api3Chains.hardhatEtherscan();
-const etherscan = {
-  apiKey: Object.entries(etherscanApiKey).reduce((populatedApiKey, etherscanApiKeyEntry) => {
-    const hardhatEtherscanChainAlias = etherscanApiKeyEntry[0];
-    const chainAlias = etherscanApiKeyEntry[1];
-    if (chainAlias !== 'DUMMY_VALUE') {
-      const envVariableName = `ETHERSCAN_API_KEY_${chainAlias}`;
-      populatedApiKey[hardhatEtherscanChainAlias] = process.env[envVariableName] ? process.env[envVariableName] : '';
-    } else {
-      populatedApiKey[hardhatEtherscanChainAlias] = 'DUMMY_VALUE';
-    }
-    return populatedApiKey;
-  }, {}),
-  customChains: etherscanCustomChains,
-};
-
-const networks = Object.entries(api3Chains.hardhatConfigNetworks()).reduce((networksWithMnemonic, networkEntry) => {
-  const chainAlias = networkEntry[0];
-  const network = networkEntry[1];
-  networksWithMnemonic[chainAlias] = {
-    ...network,
-    accounts: { mnemonic: process.env.MNEMONIC ? process.env.MNEMONIC : '' },
-  };
-  return networksWithMnemonic;
-}, {});
-
 module.exports = {
-  etherscan,
-  networks,
+  etherscan: hardhatConfig.etherscan(),
+  networks: hardhatConfig.networks(),
   solidity: {
     version: '0.8.17',
     settings: {
