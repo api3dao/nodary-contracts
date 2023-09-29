3dd841ed690b8d6ec90ec35694af1eb725c447e8
bbenligiray <bbenligiray@gmail.com>
2023-09-29 19:27:01 +0300

    Bump solc back down to fix tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,11 @@ module.exports = {
   etherscan: hardhatConfig.etherscan(),
   networks: hardhatConfig.networks(),
   solidity: {
-    version: '0.8.20',
+    version: '0.8.17',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 1000,
       },
     },
   },
